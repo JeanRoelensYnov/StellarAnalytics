34a465c229034cee2721e5f00e687d85dd2b16fe
J.Roelens <jeanroelens31@gmail.com>
2024-07-08 09:50:21 +0200

    [Documentation] Added some description under each scrapper function.

--- StellarAnalytics/Data/Scrapper/scrapper.py
+++ StellarAnalytics/Data/Scrapper/scrapper.py
@@ -4,6 +4,11 @@ from ..Requests_tools import request_tools
 from locale import atof
 
 def url_builder() -> dict:
+    """
+    Get the common start and end of url necessary for data retrieving, then buil url by concatenation with
+    the chemical symbol and ionization like this : START_URL + SYMBOL + IONIZATION + END_URL.
+    Return a dict with SYMBOL IONIZATION as key and CONCATENATED_URL as value.
+    """
     resp = {}
     sUrl, eUrl, eDict = request_tools.load_const()
     for key in eDict.keys():
@@ -12,6 +17,9 @@ def url_builder() -> dict:
     return resp
 
 def get_table(req : Response):
+    """
+    Parser help to get the correct table containing the data in which we are interested in.
+    """
     soup = BeautifulSoup(req.text,'html.parser')
     tables = soup.find_all('table')
     targetTable = [table for table in tables if 'background-color:#FFFEEE;' in table.get('style', '')]
@@ -21,9 +29,16 @@ def get_table(req : Response):
         return None
 
 def get_tbody(table):
+    """
+    Parser help to get the correct tbody containing the data in which we are interested in.
+    """
     return table.find_all('tbody')[1]
 
 def get_tbody_content(tbody) -> tuple[list, list]:
+    """
+    Mashup of all the scrapper logic, for a given tbody will return a tuple containing two list the first one
+    being the observed wavelength and the second list is realtive intensities.
+    """
     observedWavelength = []
     relativeIntensities = []
     for tr in tbody.find_all('tr'):
