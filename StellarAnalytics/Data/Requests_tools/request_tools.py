cc2f4291071041c2dabd1ef9c9ada651618a4486
J.Roelens <jeanroelens31@gmail.com>
2024-07-08 09:11:32 +0200

    [GIT/Documentation] Added a gitignore file for better readability, and some function documentation in request_tools.

--- StellarAnalytics/Data/Requests_tools/request_tools.py
+++ StellarAnalytics/Data/Requests_tools/request_tools.py
@@ -10,6 +10,9 @@ and constant in pascal case example : SomethingWhichIsConstant
 Following the rules of clean code your functions shouldn't exceed 25 lines
 """
 def load_const() -> tuple[str, str, dict]:
+    """
+    Loader for parameter present in our config.yml file.
+    """
     configPath = Path(__file__).parent.parent / 'config.yml'
     if not configPath.is_file():
         raise FileNotFoundError(f"Config file not found at: {configPath}")
@@ -22,9 +25,15 @@ def load_const() -> tuple[str, str, dict]:
     return sUrl, eUrl, eDict
 
 def req(URL) -> requests.models.Response:
+    """
+    QOL function to retrieve requests response.
+    """
     return requests.get(URL)
 
 def test_url(URL) -> bool:
+    """
+    Safeguard to check if a URL is correctly formatted or if it doesn't return a status code other than 200.
+    """
     if req(URL).status_code == 200 :
         return True
     return False
